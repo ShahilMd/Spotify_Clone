7c889443e5a4ab85232c0e0dada844a242836d05
ShahilMd <shahil.connect@gmail.com>
2025-09-01 21:38:55 +0530

    Initialize project

---
+++ UserService/src/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 5000;
+app.listen(5000, () => {
+    console.log(`server is running ${port}`);
+});
+//# sourceMappingURL=index.js.map
---
+++ UserService/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+
+
+const app = express();
+const port = process.env.PORT || 5000
+
+app.listen(5000, () => {
+  console.log(`server is running ${port}`);
+})
